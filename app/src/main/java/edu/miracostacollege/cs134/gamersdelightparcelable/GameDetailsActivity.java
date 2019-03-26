03665ca14470cbdf8bcbfbecde0c7e476e32fd2a
4803AW16 <4803AW16@usa.miracosta.cc.ca.us>
2019-03-26 15:05:35 -0700

    Initial commit

---
+++ app/src/main/java/edu/miracostacollege/cs134/gamersdelightparcelable/GameDetailsActivity.java
@@ -0,0 +1,58 @@
+package edu.miracostacollege.cs134.gamersdelightparcelable;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import edu.miracostacollege.cs134.gamersdelightparcelable.model.Game;
+
+
+public class GameDetailsActivity extends AppCompatActivity {
+
+    private static final String TAG = GameDetailsActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_details);
+
+        ImageView gameDetailsImageView = findViewById(R.id.gameDetailsImageView);
+        TextView gameDetailsNameTextView = findViewById(R.id.gameDetailsNameTextView);
+        TextView gameDetailsDescriptionTextView = findViewById(R.id.gameDetailsDescriptionTextView);
+        RatingBar gameDetailsRatingBar = findViewById(R.id.gameDetailsRatingBar);
+
+        Intent detailsIntent = getIntent();
+        //Retrieve a game object from the intent
+        Game game = detailsIntent.getParcelableExtra("SelectedGame");
+
+        /*String name = detailsIntent.getStringExtra("Name");
+        String description = detailsIntent.getStringExtra("Description");
+        float rating = detailsIntent.getFloatExtra("Rating", 0.0f);
+        String imageName = detailsIntent.getStringExtra("ImageName");*/
+
+
+        AssetManager am = getAssets();
+        try {
+            InputStream stream = am.open(game.getImageName());
+            Drawable image = Drawable.createFromStream(stream, game.getName());
+            gameDetailsImageView.setImageDrawable(image);
+        }
+        catch (IOException ex)
+        {
+            Log.e(TAG, "Error loading: " + game.getImageName(), ex);
+        }
+
+        gameDetailsNameTextView.setText(game.getName());
+        gameDetailsDescriptionTextView.setText(game.getDescription());
+        gameDetailsRatingBar.setRating(game.getRating());
+    }
+}
